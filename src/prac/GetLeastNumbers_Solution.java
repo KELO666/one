881038fb2d9b0433d9f7d1ffe1cb033d2eafd8c7
KELO666 <2237591924@qq.com>
2019-06-13 23:40:47 +0800

    剑指offer一刷（待续）

--- src/prac/GetLeastNumbers_Solution.java
+++ src/prac/GetLeastNumbers_Solution.java
@@ -1,6 +1,7 @@
 package prac;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * @author KELO
@@ -8,11 +9,17 @@ import java.util.ArrayList;
  * 输入n个整数，找出其中最小的K个数。
  * 例如输入4,5,1,6,2,7,3,8这8个数字，则最小的4个数字是1,2,3,4,。
  *
- *
+ * 排序，找前K个
  */
 public class GetLeastNumbers_Solution {
     public ArrayList<Integer> GetLeastNumbers_Solution(int [] input, int k) {
-
-        return null;
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        if (input==null||k<1) return list;
+        if (input.length<k) return list;
+        Arrays.sort(input);
+        for (int i=0;i<k;i++){
+            list.add(input[i]);
+        }
+        return list;
     }
 }
